b0dc686beaf2f00c1542c535c8ba11a86ea38848
uxl <brian@ulx.io>
2015-11-13 21:30:32 -0500

    smashed bugs - dweet makes sense for iot but not so much for games. right?

--- www/js/pairing.js
+++ www/js/pairing.js
@@ -1,55 +1,56 @@
 'use strict';
 
-var PAIRING = (function($) {
+var GUTS = (function($) {
     //vars
     var gameID = null,
         currentPlayer = null,
-        player1score, 
-        player2score,        
-        player1name, 
+        player1score,
+        player2score,
+        player1name,
         player2name,
         makeID = function(callback) {
             console.log('makeID');
             var ID = Math.floor(Math.random() * 9999);
             checkIfGame(ID);
-            
+
         },
-        setPlayer1= function(name){
+        setPlayer1 = function(name) {
             player1name = name;
             currentPlayer = name;
         },
-        setPlayer2 = function(name){
+        setPlayer2 = function(name) {
             player2name = name;
             currentPlayer = name;
         },
         checkIfGame = function(ID) {
             console.log('checkIfGame');
 
-           $.ajax({
+
+            $.ajax({
 
                 url: "https://dweet.io/get/dweets/for/" + ID + ".json",
-             
+
                 // The name of the callback parameter, as specified by the YQL service
                 jsonp: "callback",
-             
+
                 // Tell jQuery we're expecting JSONP
                 dataType: "jsonp",
 
                 method: 'GET',
-             
+
                 // Work with the response
-                success: function( response ) {
-                     if (response.with == 404) {
-                    //show the button to move to 
-                    buildGame(ID);
-                } else {
-                    buildGame();
-                }
+                success: function(response) {
+                    if (response.with == 404) {
+                        //show the button to move to 
+                        buildGame(ID);
+                    } else {
+                        buildGame();
+                    }
                 },
                 fail: function(errorThrown) {
-                console.log("error: " + error)
+                    console.log("error: " + error)
                 }
-            });            
+            });
         },
         buildGame = function(ID) {
             console.log('buildGame');
@@ -59,25 +60,27 @@ var PAIRING = (function($) {
             };
             console.log('buildGame: ' + ID)
             gameID = ID;
-            updateThing({startedGame:true});
+            createThing({
+                startedGame: true
+            });
 
         },
         getGame = function(myID) {
 
-           $.ajax({
+            $.ajax({
                 url: "https://dweet.io/get/latest/dweet/for/" + myID + ".json",
-             
+
                 // The name of the callback parameter, as specified by the YQL service
                 jsonp: "callback",
-             
+
                 // Tell jQuery we're expecting JSONP
                 dataType: "jsonp",
 
                 method: 'GET',
-             
+
                 // Work with the response
-                success: function( response ) {
-                    console.log( response ); // server response
+                success: function(response) {
+                    console.log(response); // server response
                     $('body').append("return: " + JSON.stringify(response));
                 },
                 fail: function(errorThrown) {
@@ -87,43 +90,39 @@ var PAIRING = (function($) {
         },
         startGame = function() {
             //getscore
-            fight(function(score){
-                updateThing({score:score, player: currentPlayer});
+            fight(function(score) {
+                createThing({
+                    score: score,
+                    player: currentPlayer
+                });
             });
         },
-        getGameID = function(){
-            return gameID;
-        },
-        updateThing = function(content){
+        createThing = function(ID) {
+            console.log('creatThing');
             $.ajax({
-                url: "https://dweet.io/get/latest/dweet/for/" + gameID + ".json",
-             
+                url: "https://dweet.io/dweet/for/" + ID + ".json",
+
                 // The name of the callback parameter, as specified by the YQL service
                 jsonp: "callback",
-             
+
                 // Tell jQuery we're expecting JSONP
                 dataType: "jsonp",
 
                 method: 'POST',
-             
-                // Tell YQL what we want and that we want JSON
-                data: {content:content},
-             
+
                 // Work with the response
-                success: function( response ) {
-                    console.log( response ); // server response
+                success: function(response) {
+                    console.log(response); // server response
                     $('body').append("return: " + JSON.stringify(response));
                 },
                 fail: function(errorThrown) {
                     console.log("error: " + error);
                 }
             });
+        };
             //fight with callback send string
-        }
     return {
         buildGame: buildGame,
-        getGame: getGame,
-        updateThing: updateThing,
-        getGameID: getGameID
+        getGame: getGame
     };
 }(jQuery));
